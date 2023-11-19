e69fab7e473adb6367cbf9eec62cfc5e8100825d
Oli <oli.varnavska@gmail.com>
2023-11-19 22:28:42 +0800

    Add seminar 1

---
+++ Task1/task3.py
@@ -0,0 +1,64 @@
+# a = int(input("Введите количество учеников в первом классе"))
+# b = int(input("Введите количество учеников во втором классе"))
+# c = int(input("Введите количество учеников в третьем классе"))
+# a1 = a // 2 + a % 2
+# b1 = b // 2 + b %2
+# c1 = c // 2 + c % 2
+# print(a1 + b1 + c1)
+
+
+# n = 123
+# n1 = n % 10
+# n2 = (n % 100) // 10
+# n3 = n // 100  
+# # print(f"{n3} + {n2} + {n1}")
+# res = n1 + n2 + n3
+# print (res)
+
+# n = 120
+# S = int (n / 6)
+# P = S
+# K = int(2 * (S + P))
+# print(S, K , P)
+
+# n = 6
+# m = n // 6
+# print(m, m * 4, m)
+
+
+# n1 = int(n // 6)
+# n2 = int(n // 6)
+# n3 = int((n // 6) * 4)
+# print(n1, n3 , n2)
+
+# n = 385916
+# n1 = n // 100000
+# n2 = (n % 100000) // 10000
+# n3 = (n % 10000) // 1000
+# n4 = (n % 1000) // 100
+# n5 = (n % 100) // 10
+# n6 = n % 10
+# if n1 + n2 + n3 == n4 + n5 + n6:
+#     print('yes')
+# else :
+#     print('no')
+
+# a = 8
+# b = 7
+# c = 156
+# if c < a * b and (c % b == 0 or c % a == 0):
+#     print('YES')
+# else:
+#     print('NO')
+
+
+# import math 
+
+
+# speed = int(input('Введите скорость: '))
+# distance = int(input('Введите расстояние: '))
+# time = math.ceil(distance/speed)
+# print(f'требуется дней: {time}')
+
+
+
